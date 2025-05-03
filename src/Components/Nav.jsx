9f359be84504457a8c48ef971c143a843c1049f4
7777Satish <onlinegamespartner2@gmail.com>
2025-05-03 15:19:05 +0530

    Addedd Header

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,52 +5,55 @@ import { FaXTwitter } from 'react-icons/fa6';
 
 function Nav(){
     return <>
-        <nav className={styles.nav}>
-            <div className={styles.logo}>
-                <img alt='FluxIDE' src='logo.png'/>
-                <h1>FluxIDE</h1>
-            </div>
-
-            <ul className={styles.links}>
-                <NavLink 
-                to="/" 
-                className={({ isActive }) => isActive ? "active" : undefined}
-                >
-                    <li>Home</li>
-                </NavLink>
-                
-                <NavLink 
-                to="/" 
-                className={({ isActive }) => isActive ? "active" : undefined}
-                >
-                    <li>About</li>
-                </NavLink>
-                
-                <NavLink 
-                to="/" 
-                className={({ isActive }) => isActive ? "active" : undefined}
-                >
-                    <li>Features</li>
-                </NavLink>
-                
-                <NavLink 
-                to="/" 
-                className={({ isActive }) => isActive ? "active" : undefined}
-                >
-                    <li>Pricing</li>
-                </NavLink>
-            </ul>
-
-            <div className={styles.right}>
-                <a href='/docs'>Docs</a>
-                <div className={styles.socials}>
-                    <button><FaDiscord /></button>
-                    <button><FaXTwitter /></button>
-                    <button><FaGithub /></button>
+        <div className={styles.nav_container}>
+            <nav className={styles.nav}>
+                <div className={styles.logo}>
+                    <img alt='FluxIDE' src='logo.png'/>
+                    <h1>FluxIDE</h1>
                 </div>
-            </div>
 
-        </nav>
+                <ul className={styles.links}>
+                    <NavLink 
+                    to="/" 
+                    className={({ isActive }) => isActive ? "active" : undefined}
+                    >
+                        <li>Home</li>
+                    </NavLink>
+                    
+                    <NavLink 
+                    to="/" 
+                    className={({ isActive }) => isActive ? "active" : undefined}
+                    >
+                        <li>About</li>
+                    </NavLink>
+                    
+                    <NavLink 
+                    to="/" 
+                    className={({ isActive }) => isActive ? "active" : undefined}
+                    >
+                        <li>Features</li>
+                    </NavLink>
+                    
+                    <NavLink 
+                    to="/" 
+                    className={({ isActive }) => isActive ? "active" : undefined}
+                    >
+                        <li>Pricing</li>
+                    </NavLink>
+                </ul>
+
+                <div className={styles.right}>
+                    <a href='/docs'>Docs</a>
+                    <a href='/download'>Download</a>
+                    <div className={styles.socials}>
+                        <button><FaDiscord /></button>
+                        <button><FaXTwitter /></button>
+                        <button><FaGithub /></button>
+                    </div>
+                </div>
+
+            </nav>
+        </div>
     </>
 }
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,22 @@
+import { FaArrowRight, FaLinux } from 'react-icons/fa';
 import Nav from './Components/Nav';
 import styles from './Home.module.css';
+import { BsUbuntu } from 'react-icons/bs';
 
 function Home(){
     return <>
         <Nav/>
+
+        <header className={styles.header}>
+            <span>The Code Editor You Want <FaArrowRight/></span>
+            <h1>Rewriting the Way You Code</h1>
+            <p>An AI-powered code editor that adapts to your style</p>
+
+            <div className={styles.buttons}>
+                <button><BsUbuntu/> Download for linux</button>
+                <button>Learn More</button>
+            </div>
+        </header>
     </>
 }
 
